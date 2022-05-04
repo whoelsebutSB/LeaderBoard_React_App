1d2e22404fe97e08c7c07118bed00920b3f1924d
whoelsebutSB <95988235+whoelsebutSB@users.noreply.github.com>
2022-05-04 12:00:08 -0400

    first commit

--- src/components/board.js
+++ src/components/board.js
@@ -16,8 +16,6 @@ export default function Board() {
         <h1 className='leaderboard'>Leaderboard</h1>
 
         <div className="duration">
-            <button onClick={handleClick} data-id='7'>7 Days</button>
-            <button onClick={handleClick} data-id='30'>30 Days</button>
             <button onClick={handleClick} data-id='0'>All-Time</button>
         </div>
 
