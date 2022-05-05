c934fbccd0507e1d7393a8ac3395f4dfa2f6d029
whoelsebutSB <95988235+whoelsebutSB@users.noreply.github.com>
2022-05-05 11:14:11 -0400

    commit

--- src/components/database.js
+++ src/components/database.js
@@ -2,21 +2,22 @@ export const Leaderboard = [
     {
         name: "@whoelsebutSB",
         location: "https://solscan.io/address/Bj7tuUrcmWgXWZsq3JrWYXqBgf8YQnTXM3zaCdwvZjQ6ndia",
-        score : "3 SOL",
+        wallet: "Bj7tuUrcmWgXWZsq3JrWYXqBgf8YQnTXM3zaCdwvZjQ6ndia",
+        score : 3,
         img: "https://i.ibb.co/rbp0s7H/logo192.png",
         dt: "2022-05-03"
     },
     {
         name: "@TEST2",
         location: "https://solscan.io/address/Bj7tuUrcmWgXWZsq3JrWYXqBgf8YQnTXM3zaCdwvZjQ6ndia",
-        score : "2 SOL",
+        score : 2,
         img: "https://i.ibb.co/rbp0s7H/logo192.png",
         dt: "2022-05-03"
     },
     {
         name: "@TEST3",
         location: "https://solscan.io/address/Bj7tuUrcmWgXWZsq3JrWYXqBgf8YQnTXM3zaCdwvZjQ6ndia",
-        score : "1 SOL",
+        score : 3.3,
         img: "https://i.ibb.co/rbp0s7H/logo192.png",
         dt: "2022-05-03"
     }
